142ebac6b73c40c5c38ca664470d2e94fedb1c66
Alessandro Razeto <razeto@lngs.infn.it>
2013-01-20 15:57:50 -0600

    Added jobid, mutex blocking methods and fixed prototypes with a more meaningful parameters descriptions. Fixed opcodes and data_width for use with ftdi_double

--- delib.cc
+++ delib.cc
@@ -7,6 +7,7 @@
 
 
 delib::value_t delib::ping (value_t count) {
+  std::lock_guard<std::mutex> lk(mutex_);
   auto x = ping_ (count);
 #ifdef DEBUG_DELIB
   std::cout << "delib::ping(" << std::hex << count << "): " << x << std::endl;
@@ -15,22 +16,25 @@ delib::value_t delib::ping (value_t count) {
 }
 
 void delib::write (data_width dw, address_t address, value_t value) {
+  std::lock_guard<std::mutex> lk(mutex_);
   write_ (dw, address, value); 
 #ifdef DEBUG_DELIB
   std::cout << "delib::write(" << dw << "," << std::hex << address << "," << std::hex << value << ")" << std::endl;
 #endif
 }
 
-void delib::write (address_t address, address_t adress_depth, address_t increments, void *buff, size_t buff_len) {
-  write_ (address, adress_depth, increments, buff, buff_len);
+void delib::write (address_t address, void *matrix, address_t columns, address_t rows) {
+  std::lock_guard<std::mutex> lk(mutex_);
+  write_ (address, matrix, columns, rows); 
 #ifdef DEBUG_DELIB
-  std::cout << "delib::write(" << std::hex << address << "," << std::hex << "," adress_depth << "," << increments << ")" << std::endl;
-  for (int i = 0; i < buff_len; i++) std::cout << std::hex << int((char*(buff))[i]) << " ";
+  std::cout << "delib::write(" << std::hex << address << "," << std::hex << "," columns << "," << rows << ")" << std::endl;
+  for (int i = 0; i < buff_len; i++) std::cout << std::hex << int((char*(matrix))[i]) << " ";
   std::cout << std::endl;
 #endif
 }
 
 delib::value_t delib::read (data_width dw, address_t address) { 
+  std::lock_guard<std::mutex> lk(mutex_);
   auto x = read_ (dw, address); 
 #ifdef DEBUG_DELIB
   std::cout << "delib::read(" << dw << "," << std::hex << address << "): " << x << std::endl;
@@ -38,11 +42,12 @@ delib::value_t delib::read (data_width dw, address_t address) {
   return x;
 }
 
-void delib::read (address_t address, address_t adress_depth, address_t increments, void *buff, size_t buff_len) {
-  read_ (address, adress_depth, increments, buff, buff_len);
+void delib::read (address_t address, void *matrix, address_t columns, address_t rows) {
+  std::lock_guard<std::mutex> lk(mutex_);
+  read_ (address, matrix, columns, rows);
 #ifdef DEBUG_DELIB
-  std::cout << "delib::read(" << std::hex << address << "," << std::hex << "," adress_depth << "," << increments << ")" << std::endl;
-  for (int i = 0; i < buff_len; i++) std::cout << std::hex << int((char*(buff))[i]) << " ";
+  std::cout << "delib::read(" << std::hex << address << "," << std::hex << "," columns << "," << rows << ")" << std::endl;
+  for (int i = 0; i < buff_len; i++) std::cout << std::hex << int((char*(matrix))[i]) << " ";
   std::cout << std::endl;
 #endif
 }
--- delib.hh
+++ delib.hh
@@ -2,31 +2,47 @@
 #define DELIB_H
 #include <stdint.h>
 #include <stddef.h>
+#include <mutex>
 
 class delib {
   public:
     typedef uint32_t address_t;
     typedef uint64_t value_t;
 
-    delib () {}
+    delib (): job_id_(1) {}
     virtual ~delib () {}
 
     value_t ping (value_t count);
 
-    enum data_width { d1, d2, d4, d8 };
+    enum data_width { d1 = sizeof (uint8_t), d2 = sizeof (uint16_t), d4 = sizeof (uint32_t), d8 = sizeof (uint64_t) };
 
     void write (data_width dw, address_t address, value_t value);
-    void write (address_t address, address_t adress_depth, address_t increments, void *buff, size_t buff_len);
+    void write (address_t address, void *matrix, address_t columns, address_t rows);
 
     value_t read (data_width dw, address_t address);
-    void read (address_t address, address_t adress_depth, address_t increments, void *buff, size_t buff_len);
+    void read (address_t address, void *matrix, address_t columns, address_t rows);
 
+    int job_id () const { return job_id_; }
   private:
     virtual value_t ping_ (value_t count) = 0;
     virtual void write_ (data_width dw, address_t address, value_t value) = 0;
-    virtual void write_ (address_t address, address_t adress_depth, address_t increments, void *buff, size_t buff_len) = 0;
+    virtual void write_ (address_t address, void *matrix, address_t columns, address_t rows) = 0;
     virtual value_t read_ (data_width dw, address_t address) = 0;
-    virtual void read_ (address_t address, address_t adress_depth, address_t increments, void *buff, size_t buff_len) = 0;
+    virtual void read_ (address_t address, void *matrix, address_t columns, address_t rows) = 0;
+
+    int job_id_;
+    std::mutex mutex_;
+
+  protected:
+    int job_id_inc () { return job_id_++; }
+
+    enum opcode {
+      oc_ping = 0x12,
+      oc_send = 0x23,
+      oc_recv = 0x34,
+      oc_send_block = 0x45,
+      oc_recv_block = 0x56,
+    };
 };
 
 #endif

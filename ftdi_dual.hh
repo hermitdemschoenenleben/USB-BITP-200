cb565eb4686c4dae355fbc9e32c8bc4b2c89d946
Alessandro Razeto <razeto@lngs.infn.it>
2013-01-20 19:30:45 -0600

    Added ftdi_single/dual proxies

---
+++ ftdi_dual.hh
@@ -0,0 +1,11 @@
+#ifndef FTDI_DUAL_H
+#define FTDI_DUAL_H
+
+#include "ftdi_base.hh"
+
+class ftdi_dual: public ftdi_base {
+  public:
+    ftdi_dual (int id, int nr = 0): ftdi_base(id, nr, B) {}
+};
+
+#endif
---
+++ ftdi_single.hh
@@ -0,0 +1,11 @@
+#ifndef FTDI_SINGLE_H
+#define FTDI_SINGLE_H
+
+#include "ftdi_base.hh"
+
+class ftdi_single: public ftdi_base {
+  public:
+    ftdi_single (int id, int nr = 0): ftdi_base(id, nr, A) {}
+};
+
+#endif
